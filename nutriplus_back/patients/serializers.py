f8a94c2651c8c0be7e010cd42f340df2e812bf39
Lucas <lucas.tnagel@gmail.com>
2019-10-18 15:16:14 -0400

    Patients modified

--- nutriplus_back/patients/serializers.py
+++ nutriplus_back/patients/serializers.py
@@ -1,18 +1,27 @@
 from rest_framework import serializers
 from .models import Patients, PatientRecord
 
+from food.models import Food
+
 
 class AddNewPatientSerializer(serializers.Serializer):
     patient = serializers.CharField(max_length=120)
     date_of_birth = serializers.DateField(format="%d/%m/%Y", input_formats=["%d/%m/%Y"])
-    food_choices = serializers.CharField(max_length=200)
-    food_restrictions = serializers.CharField(max_length=200, allow_blank=True)
+    food_restrictions = serializers.CharField(max_length=500, allow_blank=True)
+
+
+class PatientFoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Food
+        fields = ('id', 'food_name', 'food_group')
 
 
 class PatientSerializer(serializers.ModelSerializer):
+    food_restrictions = PatientFoodSerializer(read_only=True, many=True)
+
     class Meta:
         model = Patients
-        fields = ('id', 'name', 'date_of_birth', 'food_choices', 'food_restrictions')
+        fields = ('id', 'name', 'date_of_birth', 'food_restrictions')
 
 
 class PatientRecordSerializer(serializers.ModelSerializer):
