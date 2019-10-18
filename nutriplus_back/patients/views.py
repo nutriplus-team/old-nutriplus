c587313d84b736d642a27955f677311d7efa62cc
Lucas <lucas.tnagel@gmail.com>
2019-10-18 15:17:35 -0400

    Removed unnecessary line

--- nutriplus_back/patients/views.py
+++ nutriplus_back/patients/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, status, pagination
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .serializers import AddNewPatientSerializer, PatientSerializer, AddPatientRecordSerializer, PatientRecordSerializer
